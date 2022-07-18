c6e4926578075da163da76ef1b552c3f20a3c3fb
Julia P <62395626+kykolkakoldyna@users.noreply.github.com>
2022-07-18 18:25:40 -0400

    Create Bond.java

---
+++ src/day0/Bond.java
@@ -0,0 +1,13 @@
+// Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+
+// nearHundred(93) → true
+// nearHundred(90) → true
+// nearHundred(89) → false
+  
+package day0;
+public class Bond{
+public static void main(String [] args){
+System.out.print("James Bond");
+}
+}
